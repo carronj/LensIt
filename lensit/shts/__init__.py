7293be50306f4993005967257e98cb0e8049a2ac
jcarron <j.carron@sussex.ac.uk>
2018-01-11 13:18:06 +0000

    shts

--- lensit/shts/__init__.py
+++ lensit/shts/__init__.py
@@ -1,5 +1,5 @@
 """
-Based on Duncan Hanson quicklens (NB: changed from single to double precision).
+Based on Duncan Hanson quicklens (NB: changed its basic routines from single to double precision).
 https://github.com/dhanson/quicklens
 
 the shts module contains routines for performing spherical harmonic transforms (SHT)s.
@@ -7,14 +7,97 @@ for more details, please see the notes in quicklens/notes/shts
 """
 import sys
 import numpy as np
-
+import pyfftw
 import util
 
 try:
     import fsht
 except ImportError, exc:
-    sys.stderr.write("IMPORT ERROR: " + __file__ + " ({})".format(
-        exc) + ". Try running 'python setup.py install' or 'python setup.py build_ext --inplace' from the shts directory.\n")
+    sys.stderr.write("IMPORT ERROR: " + __file__ + " ({})".format(exc)
+    + ". Try running 'python setup.py install' or 'python setup.py build_ext --inplace' from the shts directory.\n")
+
+PYFFTWFLAGS = []
+
+def add_PYFFTWFLAGS(flag):
+    PYFFTWFLAGS.append(flag)
+
+def vtm2map(spin, vtm, Nphi, pfftwthreads=4, bicubic_prefilt=False, phiflip=[]):
+    """ Longitudinal Fourier transform to an ECP grid.
+    Sends vtm array to (bicubic prefiltered map) with Nphi points equidistant in [0,2pi).
+    With bicubic prefiltering this uses 2lmax + 1 1d Ntheta-sized FFTs and one 2d (Ntheta x Nphi) iFFT.
+
+    Apparently the pyFFTW.FFTW is twice as fast than the pyFFTW.interface.
+    But the FFTW wisdom calculation overhead can compensate for the gain if only one map is lensed.
+
+    vtm should come from shts.vlm2vtm which returns a farray, with contiguous vtm[:,ip].
+    for spin 0 we have vtm^* = vt_-m, real filtered maps and we may use rffts. (not done)
+
+    vtm should of size 2 * lmax + 1
+    Flipping phi amounts to phi -> 2pi - phi -> The phi fft is sent to its transpose.
+    """
+    lmax = (vtm.shape[1] - 1) / 2
+    Nt = vtm.shape[0]
+    assert (Nt, 2 * lmax + 1) == vtm.shape, ((Nt, 2 * lmax + 1), vtm.shape)
+    assert Nphi % 2 == 0,Nphi
+
+    if bicubic_prefilt:
+        a = pyfftw.empty_aligned(Nt, dtype=complex)
+        b = pyfftw.empty_aligned(Nt, dtype=complex)
+        ret = pyfftw.empty_aligned((Nt, Nphi), dtype=complex, order='C')
+        fftmap = pyfftw.empty_aligned((Nt, Nphi), dtype=complex, order='C')
+        ifft2 = pyfftw.FFTW(fftmap, ret, axes=(0, 1), direction='FFTW_BACKWARD', threads=pfftwthreads,flags = PYFFTWFLAGS)
+        fft1d = pyfftw.FFTW(a, b, direction='FFTW_FORWARD', threads=1,flags = PYFFTWFLAGS)
+        fftmap[:] = 0.
+        if Nphi > 2 * lmax:
+            # There is unique correspondance m <-> kphi where kphi is the 2d flat map frequency
+            for ip, m in enumerate(range(-lmax, lmax + 1)):
+                fftmap[:, (Nphi + m if m < 0 else m)] = fft1d(vtm[:, ip])
+        else:
+            # The correspondance m <-> k is not unique anymore, but given by
+            # (m - k) = N j for j in 0,+- 1, +- 2 ,etc. -> m = k + Nj
+            for ik in range(Nphi):
+                # candidates for m index
+                ms = lmax + ik + Nphi * np.arange(-lmax / Nphi - 1, lmax / Nphi + 1, dtype=int)
+                ms = ms[np.where((ms >= 0) & (ms <= 2 * lmax))]
+                fftmap[:, ik] = fft1d(np.sum(vtm[:, ms], axis=1))
+        w0 = Nphi * 6. / (2. * np.cos(2. * np.pi * np.fft.fftfreq(Nt)) + 4.)
+        w1 = 6. / (2. * np.cos(2. * np.pi * np.fft.fftfreq(Nphi)) + 4.)
+        fftmap[:] *= np.outer(w0, w1)
+        fftmap[phiflip, :].imag *= -1.
+        return ifft2().real if spin == 0 else ifft2()
+    else :
+        # Probably no real gain to expect here from pyfftw for the ffts.
+        # (Actually a lot in relative terms, could implement that)
+        if Nphi > 2 * lmax + 1:
+            a = np.zeros((Nt,Nphi),dtype = complex)
+            a[:,:2 * lmax + 1] = vtm
+            ret = np.fft.ifft(a) * (np.exp(np.arange(Nphi) * (-1j / Nphi * (2. * np.pi) * lmax)) * Nphi)
+        else:
+            ret = np.fft.ifft(vtm[:,lmax - Nphi/2:lmax + Nphi/2])
+            ret *= (np.exp(np.arange(Nphi) * (-1j / Nphi * (2. * np.pi) * Nphi/2)) * Nphi)
+        return ret.real if spin == 0 else ret
+
+def map2vtm(spin,lmax,_map,pfftwthreads = 1):
+    """ longitudinal Fourier transforms of an ECP grid
+    input _map is (Ntheta,Nphi)-shaped.
+    Output vtm is (Ntheta,2 * lmax + 1) shaped, with v[t,lmax + m] = vtm.
+    This assume the _map samples uniformly the longitude [0,2pi) with Nphi = _map.shape[1] points.
+    (e.g. vtm2map output)
+    """
+    assert _map.shape[1] % 2 == 0,_map.shape
+    Nt,Nphi = _map.shape
+    a = pyfftw.empty_aligned(Nphi, dtype=complex)
+    b = pyfftw.empty_aligned(Nphi, dtype=complex)
+    fft1d = pyfftw.FFTW(a, b, direction='FFTW_FORWARD', threads=pfftwthreads,flags = PYFFTWFLAGS)
+    vtm = np.zeros( (Nt,2 * lmax + 1),dtype = complex,order = 'C')
+    if Nphi <= 2 * lmax:
+        sli = slice(lmax - Nphi/2,lmax+Nphi/2)
+        for it in range(Nt): vtm[it, sli] = np.fft.fftshift(fft1d(_map[it, :]))
+        vtm[:,lmax + Nphi/2] = vtm[:,lmax - Nphi/2]
+    else : # We could conceive downsampling in some situations but who cares.
+        sli = slice(Nphi/2 - lmax ,Nphi/2 + lmax + 1)
+        for it in range(Nt): vtm[it,:] = np.fft.fftshift(fft1d(_map[it, :]))[sli]
+    return vtm * (2. * np.pi / Nphi)
 
 
 def vlm2map(s, tht, phi, vlm):
@@ -113,5 +196,26 @@ def vtm2vlm(s, tht, vtm):
     """
     This computes sum_th sLm(tht) vtm.
     This returns original vlm for input vtm * dcos(tht) * (2 pi) on a fine co-latitude grid.
+
+    Recall that \int_1^1 dcost sLlm sLl'm = 1/(2 pi) delta_{ll'}
     """
     return fsht.vtm2vlm(s, tht, vtm)
+
+def vtm2tlm_sym(tht, vtm):
+    """
+    This computes sum_th sLm(tht) vtm.
+    This returns original vlm for input vtm * dcos(tht) * (2 pi) on a fine co-latitude grid, using symetry tricks
+
+    Recall that \int_1^1 dcost sLlm sLl'm = 1/(2 pi) delta_{ll'}
+    """
+    lmax = (vtm.shape[1] - 1) /2
+    assert 2 * lmax + 1 == vtm.shape[1] and vtm.shape[0] == len(tht)
+    assert len(tht) % 2 == 0
+    assert np.all(np.sort(tht) == tht)
+    assert np.allclose(np.pi - tht[:len(tht)/2],tht[len(tht)/2:][::-1])
+    _vtm = np.zeros((len(tht),lmax + 1),dtype = complex)
+    _vtm[:len(tht) // 2,:]  = vtm[:len(tht) // 2,lmax:] + vtm[slice(len(tht), len(tht) // 2 -1, -1),lmax:]
+    _vtm[len(tht) // 2 :,:] = vtm[:len(tht) // 2,lmax:] - vtm[slice(len(tht), len(tht) // 2 -1, -1),lmax:]
+    return fsht.vtm2alm_syms0(tht[:len(tht)/2], _vtm)
+
+
