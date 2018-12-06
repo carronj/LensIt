b6ac944cc5eb896216e998b01874cd251efba07f
jcarron <j.carron@sussex.ac.uk>
2018-12-06 13:32:03 +0100

    treatment of poles

--- lensit/shts/__init__.py
+++ lensit/shts/__init__.py
@@ -16,14 +16,16 @@ except ImportError, exc:
     sys.stderr.write("IMPORT ERROR: " + __file__ + " ({})".format(exc)
     + ". Try running 'python setup.py install' or 'python setup.py build_ext --inplace' from the shts directory.\n")
 
-#FIXME: It looks like the setup.py build does not work properly with openMP.
+#FIXME: It looks like the setup.py build does not work properly with clang with openMP.
 #FIXME: Instead this seems to work: f2py -c -m fsht shts.f90 --f90flags="-fopenmp" -lgomp
-PYFFTWFLAGS = []
+
+#f2py -c -m fsht shts.f90  --f90flags="-fopenmp" -lgomp --fcompiler=gfortran
+PYFFTWFLAGS = ['FFTW_MEASURE']
 
 def add_PYFFTWFLAGS(flag):
     PYFFTWFLAGS.append(flag)
 
-def vtm2map(spin, vtm, Nphi, pfftwthreads=4, bicubic_prefilt=False, phiflip=[]):
+def vtm2map(spin, vtm, Nphi, pfftwthreads=4, bicubic_prefilt=False, phiflip=()):
     """ Longitudinal Fourier transform to an ECP grid.
     Sends vtm array to (bicubic prefiltered map) with Nphi points equidistant in [0,2pi).
     With bicubic prefiltering this uses 2lmax + 1 1d Ntheta-sized FFTs and one 2d (Ntheta x Nphi) iFFT.
@@ -41,15 +43,15 @@ def vtm2map(spin, vtm, Nphi, pfftwthreads=4, bicubic_prefilt=False, phiflip=[]):
     Nt = vtm.shape[0]
     assert (Nt, 2 * lmax + 1) == vtm.shape, ((Nt, 2 * lmax + 1), vtm.shape)
     assert Nphi % 2 == 0, Nphi
-
     if bicubic_prefilt:
+        #TODO: Could use real ffts for spin 0. For high-res interpolation this task can take about half of the total time.
         a = pyfftw.empty_aligned(Nt, dtype=complex)
         b = pyfftw.empty_aligned(Nt, dtype=complex)
         ret = pyfftw.empty_aligned((Nt, Nphi), dtype=complex, order='C')
         fftmap = pyfftw.empty_aligned((Nt, Nphi), dtype=complex, order='C')
-        ifft2 = pyfftw.FFTW(fftmap, ret, axes=(0, 1), direction='FFTW_BACKWARD', threads=pfftwthreads,flags = PYFFTWFLAGS)
+        ifft2 = pyfftw.FFTW(fftmap, ret, axes=(0, 1), direction='FFTW_BACKWARD', threads=pfftwthreads,flags=PYFFTWFLAGS)
         fft1d = pyfftw.FFTW(a, b, direction='FFTW_FORWARD', threads=1,flags = PYFFTWFLAGS)
-        fftmap[:] = 0.
+        fftmap[:] = 0. #NB: sometimes the operations above can result in nan's
         if Nphi > 2 * lmax:
             # There is unique correspondance m <-> kphi where kphi is the 2d flat map frequency
             for ip, m in enumerate(range(-lmax, lmax + 1)):
@@ -65,8 +67,8 @@ def vtm2map(spin, vtm, Nphi, pfftwthreads=4, bicubic_prefilt=False, phiflip=[]):
         w0 = Nphi * 6. / (2. * np.cos(2. * np.pi * np.fft.fftfreq(Nt)) + 4.)
         w1 = 6. / (2. * np.cos(2. * np.pi * np.fft.fftfreq(Nphi)) + 4.)
         fftmap[:] *= np.outer(w0, w1)
-        fftmap[phiflip, :].imag *= -1.
-        return ifft2().real if spin == 0 else ifft2()
+        retmap = ifft2().real if spin == 0 else ifft2()
+
     else :
         # Probably no real gain to expect here from pyfftw for the ffts.
         # (Actually a lot in relative terms, could implement that)
@@ -77,7 +79,9 @@ def vtm2map(spin, vtm, Nphi, pfftwthreads=4, bicubic_prefilt=False, phiflip=[]):
         else:
             ret = np.fft.ifft(vtm[:,lmax - Nphi/2:lmax + Nphi/2])
             ret *= (np.exp(np.arange(Nphi) * (-1j / Nphi * (2. * np.pi) * Nphi/2)) * Nphi)
-        return ret.real if spin == 0 else ret
+        retmap = ret.real if spin == 0 else ret
+    retmap[phiflip, :] = retmap[phiflip,::-1]
+    return retmap
 
 def map2vtm(spin,lmax,_map,pfftwthreads = 1):
     """ longitudinal Fourier transforms of an ECP grid
@@ -173,25 +177,86 @@ def glm2vtm_sym(s, tht, glm):
 
 def vlm2vtm(s, tht, vlm):
     """
-    This is \sum_l _s\Lambda_{lm} v_{lm}. 0 < tht < np.pi are colatitudes .
+    This is \sum_l _s\Lambda_{lm} v_{lm}. 0 < tht < pi are colatitudes .
     """
     assert s >= 0
     lmax = int(np.sqrt(len(vlm)) - 1)
     assert (len(vlm) == (lmax + 1) ** 2)
-    if s == 0: print "Consider using glm2vtm for spin 0 for factor of 2 speed-up"
-    return fsht.vlm2vtm(lmax, s, tht, vlm)
-
+    if s == 0:
+        print "Consider using glm2vtm for spin 0 for factor of 2 speed-up"
+        return fsht.vlm2vtm(lmax, s, tht, vlm)
+    #: resolving poles, since fsht implementation of spin > 0 does not handle them.
+    north = np.where(tht <= 0.)[0]
+    south = np.where(tht >= np.pi)[0]
+    if len(north) == 0 and len(south) == 0:
+        return fsht.vlm2vtm(lmax, s, tht, vlm)
+    else:
+        ret = np.zeros((len(tht), 2 * lmax + 1), dtype=complex)
+        if len(north) > 0:
+            ret[north] = _vlm2vtm_northpole(s, vlm)
+        if len(south) > 0:
+            ret[south] = _vlm2vtm_southpole(s, vlm)
+        if len(north) + len(south) < len(tht):
+            others = np.where((tht < np.pi) & (tht > 0.))[0]
+            ret[others] = fsht.vlm2vtm(lmax, s, tht[others], vlm)
+        return ret
 
 def vlm2vtm_sym(s, tht, vlm):
     """
-    This is \sum_l _s\Lambda_{lm} v_{lm}.
-    tht colatitude.
+    This is \sum_l _s\Lambda_{lm} v_{lm}. tht colatitude.
     """
     assert s >= 0
+    tht = np.array(tht)
     lmax = int(np.sqrt(len(vlm)) - 1)
     assert (len(vlm) == (lmax + 1) ** 2)
-    if s == 0: print "Consider using glm2vtm_sym for spin 0 for factor of 2 speed-up"
-    return fsht.vlm2vtm_sym(lmax, s, tht, vlm)
+    if s == 0:
+        print "Consider using glm2vtm_sym for spin 0 for factor of 2 speed-up"
+        return fsht.vlm2vtm_sym(lmax, s, tht, vlm)
+    else:
+        #: resolving poles, since fsht implementation does not handle them.
+        north = np.where(tht <= 0.)[0]
+        south = np.where(tht >= np.pi)[0]
+        if len(north) == 0 and len(south) == 0:
+            return fsht.vlm2vtm_sym(lmax, s, tht, vlm)
+        else:
+            nt = len(tht)
+            ret = np.zeros( (2 * nt, 2 * lmax + 1), dtype=complex)
+            if len(north) > 0:
+                ret[north] = _vlm2vtm_northpole(s, vlm)
+                ret[nt + north] = _vlm2vtm_southpole(s, vlm)
+            if len(south) > 0:
+                ret[south] = _vlm2vtm_southpole(s, vlm)
+                ret[nt + south] = _vlm2vtm_northpole(s, vlm)
+            if len(north) + len(south) < len(tht):
+                others = np.where( (tht < np.pi) & (tht > 0.))[0]
+                vtm =  fsht.vlm2vtm_sym(lmax, s, tht[others], vlm)
+                ret[others] = vtm[:len(others)]
+                ret[nt + others] = vtm[len(others):]
+            return ret
+
+def _vlm2vtm_northpole(s, vlm):
+    """On the north pole, the spherical harmonics obeys _s\Lambda_{l,-s} (-1)^s  \sqrt{ (2l + 1) / 4\pi }
+        and zero otherwise.
+    """
+    assert s >= 0, s
+    lmax = int(np.sqrt(len(vlm)) - 1)
+    assert (len(vlm) == (lmax + 1) ** 2)
+    ret = np.zeros(2 * lmax + 1, dtype=complex)
+    l = np.arange(lmax + 1)
+    ret[lmax - s] =  np.sum(vlm[l * l + l - s] * np.sqrt((2 * l + 1.))) / np.sqrt(4. * np.pi)  * (-1) ** s
+    return ret
+
+def _vlm2vtm_southpole(s, vlm):
+    """On the south pole, the spherical harmonics obeys _s\Lambda_{l,s} (-1)^l  \sqrt{ (2l + 1) / 4\pi }
+        and zero otherwise.
+    """
+    assert s >= 0, s
+    lmax = int(np.sqrt(len(vlm)) - 1)
+    assert (len(vlm) == (lmax + 1) ** 2)
+    ret = np.zeros(2 * lmax + 1, dtype=complex)
+    l = np.arange(lmax + 1)
+    ret[lmax + s] =  np.sum(vlm[l * l + l + s] * (-1) ** l * np.sqrt((2 * l + 1.))) / np.sqrt(4. * np.pi)
+    return ret
 
 
 def vtm2vlm(s, tht, vtm):
