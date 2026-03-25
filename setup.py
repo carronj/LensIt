d79418da32dbbef7cfe653c1b82631560aeb2886
Louis Legrand <louis.legrand@unige.ch>
2026-03-25 19:28:27 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 import setuptools
-from numpy.distutils.core import setup
-from numpy.distutils.misc_util import Configuration
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+# For Fortran extension support with modern Python/NumPy
+try:
+    from numpy.distutils.core import setup as numpy_setup
+    from numpy.distutils.misc_util import Configuration
+    USE_NUMPY_DISTUTILS = True
+except ImportError:
+    # NumPy 2.0+ removed numpy.distutils, use meson-python or setuptools
+    USE_NUMPY_DISTUTILS = False
+
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
@@ -11,19 +19,55 @@ def configuration(parent_package='', top_path=''):
     config.add_extension('lensit.bicubic.bicubic', ['lensit/bicubic/bicubic.f90'])
     return config
 
-setup(
+
+# Common setup arguments
+setup_args = dict(
     name='lensit',
-    packages=['lensit', 'lensit.qcinv', 'lensit.ffs_covs', 'lensit.ffs_deflect',
-              'lensit.pbs', 'lensit.misc', 'lensit.pseudocls', 'lensit.ffs_iterators'],
-    data_files=[('lensit/data/cls', ['lensit/data/cls/fiducial_flatsky_lensedCls.dat',
-                                         'lensit/data/cls/fiducial_flatsky_lenspotentialCls.dat',
-                                         'lensit/data/cls/fiducial_params_tensor.ini',
-                                          'lensit/data/cls/fiducial_tensCls.dat'])],
+    version='1.0.0',
+    packages=find_packages(include=['lensit', 'lensit.*']),
+    package_data={
+        'lensit': ['data/cls/*.dat', 'data/cls/*.ini'],
+    },
+    include_package_data=True,
     url='https://github.com/carronj/lensit',
     author='Julien Carron',
     author_email='to.jcarron@gmail.com',
     description='CMB lensing flat-sky quadratic and iterative estimation tools',
-    install_requires=['numpy', 'pyfftw', 'scipy'], #skipped mpi4py for travis
     long_description=long_description,
-    configuration=configuration)
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'numpy',
+        'pyfftw',
+        'scipy',
+    ],
+    python_requires='>=3.8',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Astronomy",
+    ],
+)
+
+if USE_NUMPY_DISTUTILS:
+    # Use numpy.distutils for Fortran extension (NumPy < 2.0)
+    setup_args['configuration'] = configuration
+    numpy_setup(**setup_args)
+else:
+    # For NumPy 2.0+, you'll need to use meson-python for Fortran extensions
+    # or pre-compile the extension. For now, install without the Fortran extension.
+    import warnings
+    warnings.warn(
+        "numpy.distutils is not available (NumPy 2.0+). "
+        "The Fortran bicubic extension will not be built. "
+        "Consider using meson-python for building Fortran extensions, "
+        "or install with NumPy < 2.0 for full functionality."
+    )
+    setup(**setup_args)
 
